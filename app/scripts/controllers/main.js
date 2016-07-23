b88a612562a61164fce9d6c24fdf335467877f47
Brian Davidson <brian.davidson.1981@gmail.com>
2016-07-23 10:49:34 -0400

    users can enter custom urls to make requests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,14 @@
  * Controller of the tnblFireApp
  */
 angular.module('tnblFireApp')
-	.controller('MainCtrl', function($scope, $timeout, $interval) {
+	.controller('MainCtrl', function($scope, $timeout, $interval, $location, $http) {
 
+		$scope.configs = [];
+		$scope.custom_request_success = false;
+
+		// https://tnbl-fire.firebaseio.com/.json
+		$scope.endpoint = '';
+		$scope.last_endpoint = '';
 		$scope.loaded = false;
 		$scope.reveal = false;
 		$timeout(function() {
@@ -21,6 +27,42 @@ angular.module('tnblFireApp')
 				$scope.loaded = !$scope.loaded;
 			}, 60000);
 		}, 100);
+
+		$scope.go = function(dest) {
+			if ($scope.endpoint.length && dest !== 'nowhere') {
+				$scope.custom_request_success = true;
+				$http({
+					method: 'GET',
+					url: $scope.endpoint
+				}).then(function successCallback(response) {
+					// this callback will be called asynchronously
+					$scope.custom_request_success = true;
+					$scope.endpoint = '';
+					// when the response is available
+					for (var config in response.data.configs) {
+						var tmp = response.data.configs[config];
+						$scope.configs.push(tmp);
+						for (var key in tmp) {
+							console.log(key + ": " + tmp[key]);
+						}
+					}
+				}, function errorCallback(response) {
+					$scope.last_endpoint = $scope.endpoint;
+					console.log("failed to receive data from: " + $scope.endpoint);
+					// called asynchronously if an error occurs
+					// or server returns response with an error status.
+				});
+			} else if (dest === 'configs') {
+				$scope.configs = [];
+				$location.path('/configs');
+			} else if (dest === 'nowhere') {
+				$scope.configs = [];
+				$scope.custom_request_success = false;
+				$scope.endpoint = '';
+				$location.path('/');
+			}
+			console.log(dest);
+		}
 
 		$scope.awesomeThings = [
 			'HTML5 Boilerplate',
