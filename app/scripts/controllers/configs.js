c30951da52e683d1d65992dbd08ab07807741d97
Brian Davidson <brian.davidson.1981@gmail.com>
2016-07-23 20:37:04 -0400

    updated some styles. user can add new configs now.

--- app/scripts/controllers/configs.js
+++ app/scripts/controllers/configs.js
@@ -11,16 +11,17 @@ angular.module('tnblFireApp')
 
 		$scope.loaded = false;
 		$scope.reveal = false;
+		$scope.creating_config = false;
 		$timeout(function() {
 			$scope.loaded = true;
 			$scope.reveal = true;
 		}, 300);
 
 		$scope.new_config = {
-			name: "host2",
-			hostname: "nessus-xml.lab.com",
-			port: 3384,
-			username: "admin"
+			name: "",
+			hostname: "",
+			port: undefined,
+			username: ""
 		};
 		// synchronize a read-only, synchronized array of messages, limit to most recent 10
 		$scope.configs = $firebaseArray(Ref.child('configs').limitToLast(10));
@@ -32,6 +33,16 @@ angular.module('tnblFireApp')
 		$scope.addConfig = function() {
 			// push a message to the end of the array
 			$scope.configs.$add($scope.new_config)
+				.then(function(data) {
+					console.dir(data);
+					$scope.new_config = {
+						name: "",
+						hostname: "",
+						port: undefined,
+						username: ""
+					};
+					$scope.creating_config = false;
+				})
 				// display any errors
 				.catch(alert);
 		};
